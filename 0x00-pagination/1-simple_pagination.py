d106f7a1899a7ae1972db8790b9ec93873f1600c
Malik <malikhussein229@gmail.com>
2024-08-19 00:13:54 +0300

    Task#1 pycodestyle

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+"""
+1-simple_pagination.py
+
+This module provides a Server class and an index_range function.
+
+The Server class is used to paginate a database of popular baby names.
+It provides a get_page method that returns a page of data based on
+the given page number and page size. The dataset method is used to
+load the data from a CSV file and cache it for subsequent calls.
+
+The index_range function is used to calculate the start and end indices
+for pagination based on the given page and page size.
+
+Author: Malik Hussein
+"""
 import csv
 import math
 from typing import List
@@ -15,7 +30,8 @@ def index_range(page, page_size):
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """
+    Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -23,7 +39,8 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """
+        Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -34,11 +51,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        
-        assert type(page) == int and page > 0
-        assert type(page_size) == int and page_size > 0
+        """
+        Returns a page of data from the dataset based
+        on the given page number and page size.
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
         self.dataset()
-        
+
         IndexRange = index_range(page, page_size)
         page_data = self.dataset()[IndexRange[0]: IndexRange[1]]
         return page_data
