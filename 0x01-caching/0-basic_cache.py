7887f716fff8d0f2ff2d7b96da1ba423eaab70c6
Malik <malikhussein229@gmail.com>
2024-08-20 21:01:00 +0300

    Task#0 Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+0-basic_cache.py
+
+This module implements a basic cache which stores data in a dictionary.
+
+Author: Malik Hussein
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def __init__(self):
+        """
+        Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item in the cache.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key.
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+
+        return self.cache_data[key]
