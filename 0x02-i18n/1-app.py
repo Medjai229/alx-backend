90fd6e4bee583e90886c54522e61918383d9a0a6
Malik <malikhussein229@gmail.com>
2024-08-27 22:15:14 +0300

    Task#1 pycodestyle

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,23 +1,42 @@
 #!/usr/bin/env python3
+"""
+File: 0-app.py
+
+A simple flask app with babel extension
+
+Author: Malik Hussein
+"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
 app = Flask(__name__)
-
 babel = Babel(app)
 
 
 class Config():
+    """
+    Class for the Flask app's configuration.
+    Used to configure Flask app's languages and timezone.
+    """
     LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en' 
+    BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app.config.from_object(Config)
 
+
 @app.route('/')
 def index():
+    """
+    Route for the homepage
+
+    This route is configured to the '/' URL.
+    It returns the rendered '0-index.html' template.
+    """
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run()
