34e524f66709c66ad114e8cf59ecb0cbf3ad559d
Malik <malikhussein229@gmail.com>
2024-08-27 20:58:31 +0300

    Task#0 Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import flask
+
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return flask.render_template('0-index.html')
+
+if __name__ == "__main__":
+    app.run()
