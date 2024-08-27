880489a782aff6f258b14f10347f796b2c002c8d
Malik <malikhussein229@gmail.com>
2024-08-27 21:41:48 +0300

    Task#0 pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+"""
+File: 0-app.py
 
+A simple flask app with babel extension
+
+Author: Malik Hussein
+"""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """
+    Route for the homepage
+
+    This route is configured to the '/' URL.
+    It returns the rendered '0-index.html' template.
+    """
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run()
