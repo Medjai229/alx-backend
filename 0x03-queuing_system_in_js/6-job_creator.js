573615df6797a14bd548068cf399bb5c48d8fe14
Malik <malikhussein229@gmail.com>
2024-09-25 15:21:25 +0300

    Task#6 Matching the data to the task

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ import { createQueue } from 'kue';
 const queue = createQueue();
 
 const jobData = {
-  phoneNumber: '123456798',
-  message: 'Data registered',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 };
 
 const job = queue.create('push_notification_code', jobData).save((err) => {
